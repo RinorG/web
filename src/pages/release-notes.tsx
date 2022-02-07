3c7e7428a1eee5658a3d8e9d0322f76544b74333
jfcurran <john@ory.sh>
2022-02-07 15:43:22 +0100

    update release-notes.tsx page, add release-list.tsx

--- src/pages/release-notes.tsx
+++ src/pages/release-notes.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import FeatureList from '../components/layouts/featurelist/feature-list'
 import HeroImage from '../components/layouts/hero/hero-image'
 import Layout from '../components/layouts/layout/layout'
 import Newsletter from '../components/layouts/newsletter/newsletter'
 import Quickstart from '../components/layouts/quickstart/quickstart'
+import ReleaseList from '../components/layouts/release-notes/release-list'
 import SEO from '../components/layouts/seo/seo'
 import * as content from '../page-content/content-release-notes'
 import { opensourceQuickstart } from '../page-content/shared/content-opensource'
@@ -13,7 +13,7 @@ const ReleasePage = () => (
   <Layout>
     <SEO {...content.seo} />
     <HeroImage {...content.hero} />
-    <FeatureList {...content.featureList} />
+    <ReleaseList {...content.featureList} />
     <Newsletter />
     <Quickstart {...opensourceQuickstart} />
   </Layout>
